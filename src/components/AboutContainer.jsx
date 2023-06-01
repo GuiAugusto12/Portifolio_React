f7da00e625780dcaac8b832d97241ff8a8e9181b
GuiAugusto12 <86721469+GuiAugusto12@users.noreply.github.com>
2023-06-01 11:27:10 -0300

    Add files via upload

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,19 @@
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>
+        Meu nome é Guilherme Augusto Ferreira Viola da Silva Coelho, estudo na Etec de Cotia
+        estou cursando o ensino médio integrado ao técnico.
+        Comecei no ano de 2021 e irer terminar no ano de 2023.
+      </p>
+      <p>
+       Curso Desenvolvimento de Sistemas, onde ao longo do curso, me apaixonei pela área de 
+       programação. Portanto desejo trabalhar nessa área, fazendo o que eu gosto.
+       Me acho uma pessoa esforçado, disciplinado, sei trabalhar em equipe.
+      </p>
+    </section>
+  );
+};
+
+export default AboutContainer;
---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,18 @@
+const ProjectsContainer = () => {
+  return (
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>
+        Ao longo do curso técnico, fiz alguns trabalhos, seja
+        usando PHP, REACT, HTML, XML, entre outros. 
+        Onde esses me fizeram gostar do curso, e querer
+        trabalhar com isso futuramente.
+      </p>
+      <a href="https://github.com" className="btn">
+        Ver Projetos
+      </a>
+    </section>
+  );
+};
+
+export default ProjectsContainer;
